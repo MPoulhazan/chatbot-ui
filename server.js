6303ec03f799e3b3b44c54e394aed99765fb3fca
Mickael Poulhazan <mickael.poulhazan@thalesgroup.com>
2020-01-07 13:08:15 +0100

    fix heroku

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/chatbotuidentist'));
+app.use(express.static(__dirname + '/dist/chatbot-ui-dentiste'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/chatbotuidentist/index.html'));
+res.sendFile(path.join(__dirname+'/dist/chatbot-ui-dentiste/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
