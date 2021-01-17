56e204436e84739bdff0bde5a17319b625ad3852
Mickaël P <mp041@hotmail.fr>
2021-01-17 18:25:09 +0100

    Task: Migration

--- src/app/components/message-form/message-form.component.spec.ts
+++ src/app/components/message-form/message-form.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { MessageFormComponent } from './message-form.component';
 
@@ -6,7 +6,7 @@ describe('MessageFormComponent', () => {
   let component: MessageFormComponent;
   let fixture: ComponentFixture<MessageFormComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ MessageFormComponent ]
     })
