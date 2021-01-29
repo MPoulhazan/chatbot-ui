8483a905e90b3f6a97b928365fd53e68119f5fe1
Mickaël P <mp041@hotmail.fr>
2021-01-29 16:02:11 +0100

    Task: Update credentials

---
+++ src/app/services/pwa-service.service.ts
@@ -0,0 +1,40 @@
+import { HostListener, Injectable } from "@angular/core";
+import { SwUpdate } from "@angular/service-worker";
+
+@Injectable()
+export class PwaService {
+  deferredPrompt: any;
+
+  constructor() {
+    this.isWithMobile();
+  }
+
+  @HostListener("window:beforeinstallprompt", ["$event"])
+  onbeforeinstallprompt(e) {
+    e.preventDefault();
+    this.deferredPrompt = e;
+  }
+
+  addToHomeScreen() {
+    if (this.deferredPrompt) {
+      this.deferredPrompt.prompt();
+
+      this.deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === "accepted") {
+          console.log("User accepted the A2HS prompt");
+        } else {
+          console.log("User dismissed the A2HS prompt");
+        }
+        this.deferredPrompt = null;
+      });
+    } else {
+      console.warn("Device is not compatible with app");
+    }
+  }
+
+  isWithMobile() {
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+      navigator.userAgent
+    );
+  }
+}
